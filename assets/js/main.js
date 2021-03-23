d1ff31ffe95db38e3db205ec1d54f4ab56fc7706
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-23 16:30:20 +0100

    ESERCIZIO + BONUS TERMINATI

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,8 @@ var app = new Vue(
   {
     el: '#root',
     data: {
-      message: "Ciao, questo è il messaggio all'interno di un h1"
+      message: "Ciao, questo è il messaggio all'interno di un h1",
+      img: 'https://lh3.googleusercontent.com/proxy/3mfqIcwYHh-t3gGITnClBV1_f97-zq4-cl1pi7NEPPG45ajg_k5dipjbCJJJMkZANFYHEJqh6NfnjkcRnEHLa83iNKLdyRt4eEoSkkp57WNOrCY1'
     }
   }
 );
